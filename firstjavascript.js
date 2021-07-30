ccfd05467c1a9cae9288d6f098d7cd84baa92e0f
vishwashcv <vishwasida46@gmail.com>
2021-07-31 04:00:06 +0530

    first commit

---
+++ firstjavascript.js
@@ -0,0 +1,24 @@
+console.log('hello world');
+/**
+ variable
+ */
+let name = "vishwash";
+let age = 19;
+/**
+object
+ */
+let person = {
+    name: 'vishwash',
+    age: 19
+};
+
+console.log(person);
+/**
+ functions 
+ */
+function square(number) {
+    return number * number;
+}
+
+
+console.log(square(2));
